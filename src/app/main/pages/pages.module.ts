69eb6b29c2b9b8a6b8528c71c039b575b2ebbe15
namreg15 <namreg.dor@gmail.com>
2023-09-23 22:47:13 -0500

    componentente página meeting

--- src/app/main/pages/pages.module.ts
+++ src/app/main/pages/pages.module.ts
@@ -5,15 +5,15 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {DashboardModule} from "../dashboard/module/dashboard.module";
 import {ProductService} from "../dashboard/services/productservice";
-import {SenseiModule} from "./sensei/module/sensei.module";
+
 import {SharedTableComponent} from "../../_shared/_components/shared-table/shared-table.component";
-import {ProductoModule} from "./producto/module/producto.module";
+
 import {TableModule} from "primeng/table";
 import { ProductCheckoutComponent } from './producto/components/product-checkout/product-checkout.component';
 import { OrderHistoryComponent } from './producto/components/order-history/order-history.component';
 import { OrderSumaryComponent } from './producto/components/order-sumary/order-sumary.component';
 import { InvoiceComponent } from './producto/components/invoice/invoice.component';
-import {StudentModule} from "./student/module/student.module";
+
 import { StudentFormComponent } from './student/components/student-form/student-form.component';
 
 @NgModule({
@@ -28,10 +28,7 @@ import { StudentFormComponent } from './student/components/student-form/student-
         FormsModule,
         DashboardModule,
 
-        SenseiModule,
-        StudentModule,
 
-        ProductoModule,
 
         TableModule,
 
---
+++ src/app/main/pages/student/components/meeting-form/meeting-form.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
+import {sdkZoomCredential} from '../../services/endpoints-student';
+
+import { ZoomMtg } from '@zoomus/websdk';
+import {endpointsStudent} from "../../services/endpoints-student";
+
+
+
+ZoomMtg.setZoomJSLib('https://source.zoom.us/2.15.2/lib', '/av');
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+// loads language files, also passes any error messages to the ui
+ZoomMtg.i18n.load('en-US');
+ZoomMtg.i18n.reload('en-US');
+
+@Component({
+    selector: 'app-meeting-form',
+    templateUrl: './meeting-form.component.html',
+    styleUrls: ['./meeting-form.component.scss']
+})
+export class MeetingFormComponent implements OnInit {
+    constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {
+    }
+
+
+    ngOnInit() {
+
+    }
+
+
+    getMeetingSinga(){
+        ZoomMtg.generateSDKSignature({
+            meetingNumber: sdkZoomCredential.meetingNumber,
+            role: sdkZoomCredential.role,
+            sdkKey: sdkZoomCredential.sdkKey,
+            sdkSecret: sdkZoomCredential.sdkSecret,
+
+            success(signature: any){
+
+                document.getElementById('zmmtg-root')!.style.display = 'block';
+                ZoomMtg.init({
+                    leaveUrl:sdkZoomCredential.leaveUrl,
+
+                    success(data: any){
+                        ZoomMtg.join({
+                            meetingNumber: sdkZoomCredential.meetingNumber,
+                            passWord: sdkZoomCredential.passWord,
+                            sdkKey: sdkZoomCredential.sdkKey,
+                            userName: sdkZoomCredential.userName,
+                            userEmail: sdkZoomCredential.userEmail,
+                            signature: signature.result,
+                            tk: '',
+
+                            success(data: any){
+                                console.log(data);
+                            },
+
+                            error(error: any){
+                                console.log('Error señalado', error);
+                            },
+                        });
+                    }
+
+                });
+
+            }, error(error2: any) {
+                console.log('Error señalado', error2);
+                alert(error2);
+
+            },
+
+
+        });
+    }
+}
+
+
+
